cdb2d23c7253be8e2c3283101e4ad9ff1ae94a62
askbk <askbk@protonmail.com>
2019-02-08 22:16:15 +0100

    Endret farger, etc.

---
+++ js/navigation.js
@@ -0,0 +1,15 @@
+// Used to toggle the menu on small screens when clicking on the menu button
+const dropdown = document.getElementById("dropdown"),
+    overlay = document.getElementById("overlay");
+
+document.getElementById("navButton").addEventListener("click", e => {
+    toggleMenu();
+});
+
+overlay.addEventListener("click", e => {
+    toggleMenu();
+})
+
+function toggleMenu() {
+    overlay.classList.toggle("w3-show");
+}
